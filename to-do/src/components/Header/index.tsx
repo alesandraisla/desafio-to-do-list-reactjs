562c3c1d823e479a21974a77c65700dc67ded41f
AlesandraIsla <alesandramempis@gmail.com>
2022-09-21 21:44:43 -0300

    Estilos globais

---
+++ to-do/src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+
+import logoToDoList from '../../assets/logoToDoList.svg';
+
+export function Header() {
+  return(
+    <header>
+      <img src={logoToDoList} alt="logo" />
+    </header>
+  )
+}
