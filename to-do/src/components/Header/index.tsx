95b53e01ee3f1bb33f20fef6dd4dad192c707cae
AlesandraIsla <alesandramempis@gmail.com>
2022-09-25 14:34:58 -0300

    Tasks e header

--- to-do/src/components/Header/index.tsx
+++ to-do/src/components/Header/index.tsx
@@ -1,10 +1,16 @@
 
 import logoToDoList from '../../assets/logoToDoList.svg';
+import styles from './header.module.css';
+import { AiOutlinePlusCircle} from 'react-icons/ai';
 
 export function Header() {
   return(
-    <header>
+    <header className={styles.header}>
       <img src={logoToDoList} alt="logo" />
+      <form className={styles.newTaskForm}>
+        <input placeholder='Adicione uma nova tarefa' />
+        <button>Criar <AiOutlinePlusCircle size={20}/></button>
+      </form>
     </header>
   )
 }
