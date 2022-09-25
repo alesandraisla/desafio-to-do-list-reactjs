46ce9258c91128b7c995835a083c3ba55b5800b0
AlesandraIsla <alesandramempis@gmail.com>
2022-09-25 20:24:41 -0300

    Resolução bug da checagem

--- to-do/src/components/Task/index.tsx
+++ to-do/src/components/Task/index.tsx
@@ -1,30 +1,32 @@
-import styles from './task.module.css';
-import { TbTrash } from 'react-icons/tb';
-import { ITask } from '../../App';
-import {BsFillCheckCircleFill} from 'react-icons/bs';
+import { TbTrash } from "react-icons/tb";
+import { BsFillCheckCircleFill } from "react-icons/bs";
+
+import styles from "./task.module.css";
+import { ITask } from "../../App";
+
 interface Props {
   task: ITask;
-  onDelete: (taskId: string) => void;
   onComplete: (taskId: string) => void;
+  onDelete: (taskId: string) => void;
 }
 
-export function Task( { task, onDelete, onComplete }: Props) {
-
-  return(
+export function Task({ task, onComplete, onDelete }: Props) {
+  return (
     <div className={styles.task}>
-        <button 
-          className={styles.checkContainer}
-          onClick={() => onComplete(task.id)}  
-        >
-          {task.isCompleted ? 
-            <BsFillCheckCircleFill /> : <div />}
-        </button>
-        <p className={task.isCompleted ? styles.textCompleted : ""}>
-          {task.title}
-        </p>
-        <button className={styles.deleteButton} onClick={() => onDelete(task.id)}>
-          <TbTrash size={20} />
-        </button>
+      <button
+        className={styles.checkContainer}
+        onClick={() => onComplete(task.id)}
+      >
+        {task.isCompleted ? <BsFillCheckCircleFill /> : <div />}
+      </button>
+
+      <p className={task.isCompleted ? styles.textCompleted : ""}>
+        {task.title}
+      </p>
+
+      <button className={styles.deleteButton} onClick={() => onDelete(task.id)}>
+        <TbTrash size={20} />
+      </button>
     </div>
-  )
+  );
 }
