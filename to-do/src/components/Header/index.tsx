228c0a2b1faae4ad077f43c0c5d112d530019068
AlesandraIsla <alesandramempis@gmail.com>
2022-09-25 19:38:23 -0300

    Deletando tarefas

--- to-do/src/components/Header/index.tsx
+++ to-do/src/components/Header/index.tsx
@@ -14,6 +14,7 @@ export function Header( { onAddTask }: Props) {
   function handleSubmit(event: FormEvent) {
     event.preventDefault();
     onAddTask(title);
+    setTitle('');
   }
 
   function onChangeTitle(event: ChangeEvent<HTMLInputElement>) {
