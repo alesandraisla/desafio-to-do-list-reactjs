5629cf7ba3bbdd7640724adc676161fa47760291
AlesandraIsla <alesandramempis@gmail.com>
2022-09-25 19:31:06 -0300

    Adicionando tarefas dinamicamente

--- to-do/src/App.tsx
+++ to-do/src/App.tsx
@@ -1,14 +1,44 @@
+import { useState } from 'react';
 import { Header } from './components/Header'
 import { Tasks } from './components/Tasks'
 
+export interface ITask {
+  id: string;
+  title: string;
+  isCompleted: boolean;
+}
+
 function App() {
+  const [ tasks, setTasks ] = useState<ITask[]>([
+    {
+      id: 'teste',
+      title: 'teste',
+      isCompleted: true,
+    },
+    {
+      id: 'teste dois',
+      title: 'teste 2',
+      isCompleted: false,
+    }
+  ]);
+
+  function addTask(taskTitle: string) {
+    setTasks([
+      ...tasks,
+      {
+        id: crypto.randomUUID(),
+        title: taskTitle,
+        isCompleted: false,
+      }
+    ])
+  }
 
   return (
     <>
-    <Header />
-    <Tasks />
+      <Header onAddTask={addTask} />
+      <Tasks tasks={tasks} />
     </>
   )
 }
 
-export default App
+export default App;
---
+++ to-do/src/components/Task/index.tsx
@@ -0,0 +1,23 @@
+import styles from './task.module.css';
+import { TbTrash } from 'react-icons/tb';
+import { ITask } from '../../App';
+
+interface Props {
+  task: ITask;
+}
+
+export function Task( { task }: Props) {
+  return(
+    <div className={styles.task}>
+        <button className={styles.checkContainer}>
+          <div />
+        </button>
+        <p>
+          {task.title}
+        </p>
+        <button className={styles.deleteButton}>
+          <TbTrash size={20} />
+        </button>
+    </div>
+  )
+}
